40f91d052a74c72426e94657d17624e4d67a0089
Raj Sagar <45603741+therajsagar@users.noreply.github.com>
2019-12-30 23:55:48 +0530

    cache static assests

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,18 @@
-console.log('%cInside Service-Worker file -> SW.js', 'background: green; color: white; display: block;')
-
-// eslint-disable-next-line no-undef
-const { skipWaiting, clientsClaim } = workbox.core
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-undef */
+const { core: { skipWaiting, clientsClaim }, precaching } = workbox
 skipWaiting()
 clientsClaim()
+
+
+self.addEventListener('install', event => {
+    console.log('Waiting to resolve.....')
+    const asyncInstall = new Promise(res => setTimeout(res, 5000))
+    event.waitUntil(asyncInstall)
+})
+
+self.addEventListener('activate', event => {
+    console.log('Activated')
+})
+
+precaching.precacheAndRoute(self.__precacheManifest || [])

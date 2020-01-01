f363586977d3bd3a819e02ec39e397a868ec9282
Raj Sagar <45603741+therajsagar@users.noreply.github.com>
2020-01-02 00:54:52 +0530

    adding routes

--- src/sw.js
+++ src/sw.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-undef */
-const { core: { skipWaiting, clientsClaim }, precaching } = workbox
+const { core: { skipWaiting, clientsClaim }, precaching, routing } = workbox
+
 skipWaiting()
 clientsClaim()
-
 
 self.addEventListener('install', event => {
     console.log('Waiting to resolve.....')
@@ -11,8 +11,22 @@ self.addEventListener('install', event => {
     event.waitUntil(asyncInstall)
 })
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', () => {
     console.log('Activated')
 })
+
+routing.registerRoute(
+    new RegExp('https:.*min\.(css|js)'),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'cdn-cache'
+    })
+)
+
+routing.registerRoute(
+    new RegExp('http://.*:4567.*\.json'),
+    workbox.strategies.networkFirst({
+        cacheName: 'api-response'
+    })
+)
 
 precaching.precacheAndRoute(self.__precacheManifest || [])

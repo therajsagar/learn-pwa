ffc017de6437a6764a69d6b2aab9779eb9616271
Raj Sagar <45603741+therajsagar@users.noreply.github.com>
2020-01-03 02:19:53 +0530

    adding navigation

--- src/App.js
+++ src/App.js
@@ -1,43 +1,75 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import logo from './logo.svg';
+import './App.css';
+
+import GreyProfile from './grey_profile.png'
+import Back from './back.png'
+
+const ITEMS_URL = "http://192.168.43.118:4567/items.json"
 const headers = { "Content-Type": "application/json" }
-const URL = "http://192.168.43.118:4567/items.json"
 
-export default class App extends Component {
+const Profile = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <span className="navbar-brand mb-0 h1">
+          <Link to="/">
+            <img src={Back} alt="logo" style={{ height: 30 }} />
+          </Link>
+          Profile
+          </span>
+      </nav>
+
+      <div style={{ textAlign: 'center' }}>
+        <img
+          src={GreyProfile} alt="profile"
+          style={{ height: 200, marginTop: 50 }}
+        />
+        <p style={{ color: '#888', fontSize: 20 }}>username</p>
+      </div>
+
+    </div>
+  )
+}
+
+
+class List extends Component {
+
   state = {
     items: [],
     loading: true,
-    todoItem: "",
-    onLine: navigator.onLine
-  };
+    todoItem: '',
+    offline: !navigator.onLine
+  }
 
   componentDidMount() {
-    fetch(URL)
+    fetch(ITEMS_URL)
       .then(response => response.json())
       .then(items => {
-        this.setState({ items, loading: false });
-      });
-    window.addEventListener('online', this.setOffineOnlineStatus)
-    window.addEventListener('offline', this.setOffineOnlineStatus)
+        this.setState({ items, loading: false })
+      })
+
+    window.addEventListener('online', this.setOfflineStatus)
+    window.addEventListener('offline', this.setOfflineStatus)
   }
 
   componentWillUnmount() {
-    window.removeEventListener('online', this.setOffineOnlineStatus)
-    window.removeEventListener('offline', this.setOffineOnlineStatus)
+    window.removeEventListener('online', this.setOfflineStatus)
+    window.removeEventListener('offline', this.setOfflineStatus)
   }
 
-
-  setOffineOnlineStatus = () => {
-    this.setState({ onLine: navigator.onLine })
+  setOfflineStatus = () => {
+    this.setState({ offline: !navigator.onLine })
   }
 
-  addItem = e => {
-    e.preventDefault();
+  addItem = (e) => {
+    e.preventDefault()
 
-    fetch(URL, {
-      method: "POST",
+    fetch(ITEMS_URL, {
+      method: 'POST',
       body: JSON.stringify({ item: this.state.todoItem }),
       headers
     })
@@ -48,14 +80,14 @@ export default class App extends Component {
         } else {
           this.setState({ items })
         }
-      });
+      })
 
-    this.setState({ todoItem: "" });
-  };
+    this.setState({ todoItem: '' })
+  }
 
-  deleteItem = itemId => {
-    fetch(URL, {
-      method: "DELETE",
+  deleteItem = (itemId) => {
+    fetch(ITEMS_URL, {
+      method: 'DELETE',
       body: JSON.stringify({ id: itemId }),
       headers
     })
@@ -66,76 +98,96 @@ export default class App extends Component {
         } else {
           this.setState({ items })
         }
-      });
-  };
-
-  handleChange = e => {
-    const todoItem = e.target.value;
-    if (todoItem.trim()) {
-      this.setState({ todoItem });
-    }
-  };
+      })
+  }
 
   render() {
-    const { todoItem, loading, items, onLine } = this.state;
     return (
       <div className="App">
         <nav className="navbar navbar-light bg-light">
           <span className="navbar-brand mb-0 h1">
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} alt="logo" />
             Todo List
           </span>
-          {!onLine && <span className="badge badge-danger my-3">Offline</span>}
+          {
+            this.state.offline &&
+            <span className="badge badge-danger my-3">
+              Offline
+            </span>
+          }
+          <span>
+            <Link to="/profile">Profile</Link>
+          </span>
         </nav>
 
         <div className="px-3 py-2">
+
           <form className="form-inline my-3" onSubmit={this.addItem}>
             <div className="form-group mb-2 p-0 pr-3 col-8 col-sm-10">
               <input
                 className="form-control col-12"
                 placeholder="What do you need to do?"
-                value={todoItem}
-                onChange={this.handleChange}
+                value={this.state.todoItem}
+                onChange={e => this.setState({
+                  todoItem: e.target.value
+                })}
               />
             </div>
             <button
               type="submit"
-              className="btn btn-primary mb-2 col-4 col-sm-2"
-            >
+              className="btn btn-primary mb-2 col-4 col-sm-2">
               Add
             </button>
           </form>
 
-          {loading ? (
-            <p>Loading...</p>
-          ) : !items.length ? (
-            <div className="alert alert-secondary">No items - all done!</div>
-          ) : (
-                <table className="table table-striped">
-                  <tbody>
-                    {items.map((item, i) => {
-                      return (
-                        <tr key={item.id} className="row">
-                          <td className="col-1">{i + 1}</td>
-                          <td className="col-10">{item.item}</td>
-                          <td className="col-1">
-                            <button
-                              type="button"
-                              className="close"
-                              aria-label="Close"
-                              onClick={() => this.deleteItem(item.id)}
-                            >
-                              <span aria-hidden="true">&times;</span>
-                            </button>
-                          </td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
-              )}
+          {this.state.loading && <p>Loading...</p>}
+
+          {
+            !this.state.loading && this.state.items.length === 0 &&
+            <div className="alert alert-secondary">
+              No items - all done!
+            </div>
+          }
+
+          {
+            !this.state.loading && this.state.items &&
+            <table className="table table-striped">
+              <tbody>
+                {
+                  this.state.items.map((item, i) => {
+                    return (
+                      <tr key={item.id} className="row">
+                        <td className="col-1">{i + 1}</td>
+                        <td className="col-10">{item.item}</td>
+                        <td className="col-1">
+                          <button
+                            type="button"
+                            className="close"
+                            aria-label="Close"
+                            onClick={() => this.deleteItem(item.id)}
+                          >
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          }
+
         </div>
+
       </div>
     );
   }
 }
+
+export default () =>
+  <Router>
+    <div>
+      <Route path="/" exact component={List} />
+      <Route path="/profile" exact component={Profile} />
+    </div>
+  </Router>

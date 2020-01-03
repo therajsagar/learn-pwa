fc17968fa9c4a1467c30863e599db764981b542a
Raj Sagar <45603741+therajsagar@users.noreply.github.com>
2020-01-04 01:59:27 +0530

    Updates

---
+++ precache-manifest.32b7e6dcea88981096bfbb18803394cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0dc5c0a8bf24ad13c6a81ede7fe30ff",
+    "url": "/learn-pwa/index.html"
+  },
+  {
+    "revision": "1eaf9204b31198d4f1cc",
+    "url": "/learn-pwa/static/css/main.47cf4e2a.chunk.css"
+  },
+  {
+    "revision": "1ed8a000b8f23ff6c4b4",
+    "url": "/learn-pwa/static/js/2.d9829571.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/learn-pwa/static/js/2.d9829571.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1eaf9204b31198d4f1cc",
+    "url": "/learn-pwa/static/js/main.b68cc2fd.chunk.js"
+  },
+  {
+    "revision": "dfd2135d4a400363cc88",
+    "url": "/learn-pwa/static/js/runtime-main.aabb40e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learn-pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,59 @@
+importScripts("/learn-pwa/precache-manifest.32b7e6dcea88981096bfbb18803394cd.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable no-useless-escape */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-undef */
+const { core: { skipWaiting, clientsClaim }, precaching, routing } = workbox
+
+skipWaiting()
+clientsClaim()
+
+self.addEventListener('install', event => {
+    console.log('Waiting to resolve.....')
+    const asyncInstall = new Promise(res => setTimeout(res, 5000))
+    event.waitUntil(asyncInstall)
+})
+
+self.addEventListener('activate', () => {
+    console.log('Activated')
+})
+
+routing.registerRoute(
+    new RegExp('https:.*min\.(css|js)'),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'cdn-cache'
+    })
+)
+
+routing.registerRoute(
+    new RegExp('http://.*:4567.*\.json'),
+    workbox.strategies.networkFirst({
+        cacheName: 'api-response'
+    })
+)
+
+self.addEventListener('fetch', event => {
+    if (event.request.method === "POST" || event.request.method === "DELETE") {
+        event.respondWith(
+            fetch(event.request).catch(err => {
+                return new Response(
+                    JSON.stringify({ error: "This action disabled while app is offline" }), {
+                    headers: { 'Content-Type': 'application/json' }
+                }
+                )
+            })
+        )
+    }
+})
+
+self.addEventListener('push', event => {
+    event.waitUntil(
+        self.registration.showNotification(
+            'TodoList',
+            { body: event.data.text() }
+        )
+    )
+})
+
+precaching.precacheAndRoute(self.__precacheManifest || [])
+
